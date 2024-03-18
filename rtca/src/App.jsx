71f5ef54cf65e5df5ebc06dba832f79a67f25ae4
GeorgesGITHUB <georgesatallahbusiness@gmail.com>
2024-03-18 00:51:22 -0400

    setup react project, setup websocket logic for front and back end

---
+++ rtca/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WebSocketComponent from './components/WebSocket'
+
+function App() {
+
+  return (
+    <>
+      <WebSocketComponent></WebSocketComponent>
+    </>
+  )
+}
+
+export default App
