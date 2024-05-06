216fd5967ca290a31b2c85699a65cbfaddb55d87
Georges A <georgesatallahbusiness@gmail.com>
2024-05-06 14:29:51 -0400

    Added UI for Login and Create page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,42 +1,89 @@
 import { useState } from 'react'
 import { Button, Stack, Typography, Input, Divider } from '@mui/joy'
 import { useNavigate } from 'react-router-dom';
+import fetchUsername from './services/validateUser';
 
 
 function Login() {
     const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [renderLogin, setRenderLogin] = useState(true)
     const navigate = useNavigate()
 
-    function handleSubmit(){
-        // Add authentication logic here
-        console.log('authentication logic missing')
-        
-        navigate('/app')
+    const toggleRenderLogin = () => setRenderLogin(prev => !prev)
+
+    async function handleLoginSubmit(){
+        // try {
+        //     const response = await fetchUsername(username)
+        //     console.log(`fetched response: ${response}`)
+        //     navigate('/app')
+        // } catch (error) {
+        //     console.error(error)
+        // }        
     }
 
     return (
-        <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={3}
-            height="50vh" // % of the viewport
+        <>
+            { renderLogin &&
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={3}
+                height="50vh" // % of the viewport
 
-        >
-            <Divider>Morse Chat</Divider>
-            <Typography level="h1">Login</Typography>
-            <Input
-                placeholder='Username'
-                onChange={e => setUsername(e.target.value)}
+            >
+                <Divider>Morse Chat</Divider>
+                <Typography level="h1">Login</Typography>
+                <Input
+                    placeholder='Username'
+                    onChange={e => setUsername(e.target.value)}
                 />
-            <Button
-                disabled={username===''}
-                onClick={handleSubmit}
-                >
-                Submit
-            </Button>
-            <Divider/>
-        </Stack>
+                <Input
+                    placeholder='Password'
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button
+                    disabled={username===''||password===''}
+                    onClick={handleLoginSubmit}
+                    >
+                    Submit
+                </Button>
+                <Button
+                    onClick={toggleRenderLogin}
+                    >
+                    Create Profile
+                </Button>
+                <Divider/>
+            </Stack>}
+            { !renderLogin &&
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={3}
+                height="50vh" // % of the viewport
+
+            >
+                <Divider>Morse Chat</Divider>
+                <Typography level="h1">Create Profile</Typography>
+                <Input
+                    placeholder='Username'
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <Input
+                    placeholder='Password'
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button
+                    disabled={username===''||password===''}
+                    //onClick={}
+                    >
+                    Submit
+                </Button>
+                <Divider/>
+            </Stack>}
+        </>
     )
 }
 
